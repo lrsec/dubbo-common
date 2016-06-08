cfc9d7724d28f60048887d906f0165f93387904f
lin.rui_984 <lin.rui_984@immomo.com>
2016-06-08 11:31:01 +0800

    update

--- src/test/java/com/momo/live/dubbo/common/extensionloader/ext6_inject/Dao.java
+++
@@ -1,6 +0,0 @@
-package com.momo.live.dubbo.common.extensionloader.ext6_inject;
-
-
-public interface Dao {
-    public void update();
-}
--- src/test/java/com/momo/live/dubbo/common/extensionloader/ext6_inject/impl/DaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.momo.live.dubbo.common.extensionloader.ext6_inject.impl;
-
-import com.momo.live.dubbo.common.extensionloader.ext6_inject.Dao;
-
-public class DaoImpl implements Dao {
-    public void update(){
-        
-    }
-}
